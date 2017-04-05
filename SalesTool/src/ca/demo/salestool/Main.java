9c1a365bccca4effad7abbf24efbca5e2a5f51d1
Daniel Eduardo <Daniel Eduardo@Daniel>
2017-04-05 14:34:44 -0500

    Initial cheking of sales tool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+package ca.demo.salestool;
+
+public final class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+
+		displayGreeting();
+		data.display();
+	}
+	private static void displayGreeting(){
+		System.out.println ("HELLOW HAPPY SALES PEOPLE!");
+		System.out.println ("HELLOW HAPPY SALES PEOPLE!");
+	}
+	}
+
+
